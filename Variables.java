7ae9239658fcad427977a532b6fff95dc3c3a5a3
Emily <hinese@rcschools.net>
2018-08-12 15:00:37 -0500

    please

--- Variables.java
+++ Variables.java
@@ -6,45 +6,43 @@
 
 public class Variables
 {
-	public static void main ( String[] args )
-	{
-		//define 1 variable of each of the
-		//following data types
-		//byte		short		int 		long
-		//float		double
-		//char      boolean		String
+    public static void main ( String[] args )
+    {
+        //please
+        //define 1 variable of each of the
+        //following data types
+        //byte      short       int         long
+        //float     double
+        //char      boolean     String
 
-		//integer variables
-		byte byteOne = 127;
-		
-		int two = 2;
-		
-		//i changed this
-		
-		//changing it again
-		
-		String whoo = "whooo";
-
-
-		//decimal variables
+        //integer variables
+        byte byteOne = 127;
+        
+        int two = 2;
+        
+        //i changed this
+        
+        //changing it again
+        
+        String whoo = "whooo";
 
 
-		//other integer types
+        //decimal variables
 
 
-		//other types
+        //other integer types
 
 
-		//output your information here
-		System.out.println("/////////////////////////////////");
-		System.out.println("*Jim Bob                08/18/06*");
-		System.out.println("*                               *");
-		System.out.println("*        integer types          *");
-		System.out.println("*                               *");
-		System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
+        //other types
 
 
-
+        //output your information here
+        System.out.println("/////////////////////////////////");
+        System.out.println("*Jim Bob                08/18/06*");
+        System.out.println("*                               *");
+        System.out.println("*        integer types          *");
+        System.out.println("*                               *");
+        System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
 
 
 
@@ -54,5 +52,8 @@ public class Variables
 
 
 
-	}
+
+
+
+    }
 }
